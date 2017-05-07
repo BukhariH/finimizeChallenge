ff1c27c3d4317eb56651592581c6af52a3ddf5af
Hasnain Bukhari <strider195@gmail.com>
2017-05-08 00:39:04 +0100

    Setup fetching currencies from server using Fetch API

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
@@ -7,7 +8,19 @@ import InterestRateInput from './components/interestRateInput';
 import CurrencyInput from './components/currencyInput';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.fetchCurrencies();
+  }
+
   render() {
+
+    let currencyDropdown = null;
+    if (this.state != null && this.state.currencies != null) {
+      let currencyList = Object.keys(this.state.currencies)
+      currencyDropdown = <CurrencyInput currencyList={currencyList} />;
+    }
+
     return (
       <div className="container">
         <form className="form-horizontal">
@@ -15,13 +28,40 @@ class App extends Component {
           <fieldset>
             <AmountInput />
             <InterestRateInput />
-            <CurrencyInput />
+            {currencyDropdown}
           </fieldset>
         </form>
         <button id="calc-btn" name="calc-btn" className="btn btn-success">Figure this all out! 🤓</button>
       </div>
     );
   }
+
+    // Get currencies and conversion rates using Fetch
+    fetchCurrencies = () => {
+        // Get currency data from our API
+        fetch("https://api-rspbfxgbum.now.sh/")
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response;
+            })
+            .then((response) => response.json())
+            .then((currencyData) => {
+                this.setState({
+                    currencies: currencyData["rates"]
+                });
+                // Set default currency
+                this.setState({
+                    currency: "AUD"
+                });
+            })
+            .catch(() => {
+                throw Error("Error parsing json then setting state")
+                });
+    };
+
 }
+
 
 export default App;
