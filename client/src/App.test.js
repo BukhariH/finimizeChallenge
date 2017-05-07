d39570e1f7ab67adfa2961f9a18927e78c3af15d
Hasnain Bukhari <strider195@gmail.com>
2017-05-07 21:40:35 +0100

    Add Enzyme & create first test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('renders the base component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.contains(<h1>Savings Calculator</h1>)).toEqual(true);
 });
