26d1906002f49e2c90a25c3a987fbcb1d6873518
Hasnain Bukhari <strider195@gmail.com>
2017-05-08 03:32:11 +0100

    Fix PropType from Function to Func

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -8,6 +8,7 @@ import AnnualEarnedInterest from './components/annualEarnedInterest';
 import MonthlyEarnedInterest from './components/monthlyEarnedInterest';
 import CalculateButton from './components/calculateButton';
 
+const emptyFunc = ()=>{};
 
 describe('<App />', () => {
   it('renders the base container', () => {
@@ -16,32 +17,32 @@ describe('<App />', () => {
   });
 
   it('renders amount input component', () => {
-    const wrapper = shallow(<AmountInput changeHandler={()=>{}} />);
+    const wrapper = shallow(<AmountInput changeHandler={emptyFunc} />);
     expect(wrapper.contains("Initial Amount")).toEqual(true);
   });
 
   it('renders interest rate component', () => {
-    const wrapper = shallow(<InterestRateInput changeHandler={()=>{}} />);
+    const wrapper = shallow(<InterestRateInput changeHandler={emptyFunc} />);
     expect(wrapper.contains("Interest Rate")).toEqual(true);
   });
 
   it('renders currency component', () => {
-    const wrapper = shallow(<CurrencyInput currencyList={["USD", "EUR"]} changeHandler={()=>{}} />);
+    const wrapper = shallow(<CurrencyInput currencyList={["USD", "EUR"]} changeHandler={emptyFunc} />);
     expect(wrapper.find('option').length).toEqual(2);
   });
 
   it('renders annual earned intrest component', () => {
-    const wrapper = shallow(<AnnualEarnedInterest earned={100} earnedConversion={110} />);
+    const wrapper = shallow(<AnnualEarnedInterest earned={"100 GBP"} earnedConversion={"110 USD"} />);
     expect(wrapper.find('input').length).toEqual(2);
   });
 
   it('renders montly earned intrest component', () => {
-    const wrapper = shallow(<MonthlyEarnedInterest earned={55} earnedConversion={60} />);
+    const wrapper = shallow(<MonthlyEarnedInterest earned={"55 GBP"} earnedConversion={"60 USD"} />);
     expect(wrapper.find('input').length).toEqual(2);
   });
 
   it('renders calculate button', () => {
-    const wrapper = shallow(<CalculateButton calcFunc={()=>{}}/>);
+    const wrapper = shallow(<CalculateButton calcFunc={emptyFunc}/>);
     expect(wrapper.contains("Figure this all out! 🤓")).toEqual(true);
   });
 });
